c9e7fa1f50ed73b9c91cf902d2ecfb6fe0363286
aidanbell <aidanbell0182@gmail.com>
2020-07-21 14:46:55 -0400

    change favicon

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Configure both serve-favicon & static middlewares
 // to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Put API routes here, before the "catch all" route
